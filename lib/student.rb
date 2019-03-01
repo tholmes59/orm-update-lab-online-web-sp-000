bca934a724d37963f85cdfb58ebfe29a329184af
Tom Holmes <tholmes59@gmail.com>
2019-03-01 00:42:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,8 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
   
-  attr_accessor :name, :grade
+  attr_accessor :name, :grade, :id 
+  
   def initialize(name, grade, id = nil)
     @name = name
     @grade = grade
