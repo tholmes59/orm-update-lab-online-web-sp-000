f4cc1473829865bd0d17371ea3a42790c8a1a72b
Tom Holmes <tholmes59@gmail.com>
2019-03-01 00:45:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,7 +18,7 @@ class Student
       CREATE TABLE IF NOT EXISTS songs (
         id INTEGER PRIMARY KEY,
         name TEXT,
-        album TEXT
+        grade TEXT
         )
         SQL
     DB[:conn].execute(sql)
