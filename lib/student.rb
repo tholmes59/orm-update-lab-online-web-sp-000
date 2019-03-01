0f6921828389415826ab230277fd526223a31021
Tom Holmes <tholmes59@gmail.com>
2019-03-01 00:57:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,4 +48,16 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
   
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
+ 
+  def self.find_by_name(name)
+    sql = "SELECT * FROM student WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Student.new(result[0], result[1], result[2])
+  end
+  
 end
