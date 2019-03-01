11634a273513934ffe702a42767472ad54140ca9
Tom Holmes <tholmes59@gmail.com>
2019-03-01 00:53:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,7 +44,8 @@ class Student
   end
 
   def update
-    
-  end 
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
   
 end
