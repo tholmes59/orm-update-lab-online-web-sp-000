12cfa85832dc6f662f110a66f20c261834d6d79e
Tom Holmes <tholmes59@gmail.com>
2019-03-01 00:44:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,16 +5,24 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
   
-  attr_accessor :name, :grade, :id 
+  attr_accessor :id, :name, :grade  
   
-  def initialize(name, grade, id = nil)
+  def initialize(id = nil, name, grade)
+    @id = id
     @name = name
     @grade = grade
-    @id = id
   end 
   
   def self.create_table
-    sql = << SQL
-    CREATE TABLE 
-
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+        )
+        SQL
+    DB[:conn].execute(sql)
+  end
+  
+  
 end
